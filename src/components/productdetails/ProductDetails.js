18ef2b8587ea909a632a5960a3aad2b280058456
David Swann <swanndavid9@gmail.com>
2024-03-26 18:28:09 -0700

    Third commit

--- src/components/productdetails/ProductDetails.js
+++ src/components/productdetails/ProductDetails.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
 import './Product-Details.css';
+import BookDetailImg from '../../assets/Books/Bar-Brawler.png';
 
 const ProductDetails = () => {
   return (
-    <section>
-      Details Section
+    <section className='ProductDetails-section-container'>
+      <div className='ProductDetails-container'>
+        <div className='ProductDetails-flex-container'>
+          <div div className='ProductDetails-BookImg-Container'>
+            <img src={BookDetailImg} alt="ProductDetailsBook" className='ProductDetailsBook' />
+          </div>
+          <div div className='ProductDetails-BookDetails-Container'>
+            <h2>BookName</h2>
+            <p>Author</p>
+            <p>Description</p>
+            <h3>Price</h3>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
